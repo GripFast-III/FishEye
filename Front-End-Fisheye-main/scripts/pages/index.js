60118205e8bc095a549eecc1ce5342cdc45867cf
GripFast-III <jeandbdp@gmail.com>
2023-04-24 17:22:11 +0200

    sauvegarde

--- Front-End-Fisheye-main/scripts/pages/index.js
+++ Front-End-Fisheye-main/scripts/pages/index.js
@@ -1,46 +1,46 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
+  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+  let photographers = [
+    {
+      name: "Ma data test",
+      id: 1,
+      city: "Paris",
+      country: "France",
+      tagline: "Ceci est ma data test",
+      price: 400,
+      portrait: "EllieRoseWilkens.jpg",
+    },
+    {
+      name: "Autre data test",
+      id: 2,
+      city: "Londres",
+      country: "UK",
+      tagline: "Ceci est ma data test 2",
+      price: 500,
+      portrait: "account.png",
+    },
+  ];
+  // et bien retourner le tableau photographers seulement une fois récupéré
+  return {
+    photographers: [...photographers], // Virer "[...photographers]" et renvoyer à la place le résultat de la fonction "fetch" que l'on aura créé avant
+  };
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
